e19eea40e6670e975faedeb270ea6c78d7ca638c
ipcrm <mcadorette@gmail.com>
2017-11-14 21:23:03 -0800

    test

--- scripts/distelli/release_me.sh
+++ scripts/distelli/release_me.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
 # Extract and move code into place, staging
-if [ -d /etc/puppetlabs/code-staging/environments/$DISTELLI_ENV ]; then
+[[ -d /etc/puppetlabs/code-staging/environments/$DISTELLI_ENV ]] && \
   sudo rm -rf /etc/puppetlabs/code-staging/environments/$DISTELLI_ENV
-  sudo mv $DISTELLI_RELVERSION /etc/puppetlabs/code-staging/environments/$DISTELLI_ENV
-else
-  sudo mv $DISTELLI_RELVERSION /etc/puppetlabs/code-staging/environments/$DISTELLI_ENV
-fi
 
+sudo mv $DISTELLI_RELVERSION /etc/puppetlabs/code-staging/environments/$DISTELLI_ENV
